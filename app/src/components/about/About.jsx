54cae6ab9b62a32c1b64fd244206fecd35c4950c
Fabien Costa <fabiencosta94800@gmail.com>
2024-10-24 12:13:34 +0200

    cursor+about anim

--- app/src/components/about/About.jsx
+++ app/src/components/about/About.jsx
@@ -1,9 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./about.scss";
 
-// Composant About
 const About = () => {
-  // État pour gérer l'affichage du texte supplémentaire (développement/réduction)
   const [isExpanded, setIsExpanded] = useState(false);
 
   // Fonction pour basculer l'état d'affichage du texte
@@ -16,7 +14,6 @@ const About = () => {
       <h1 className="about_title">A propos de moi...</h1>
       <div className="about_main">
         <div className="about_main_description">
-          {/* Section de présentation personnelle */}
           <h2 className="about_main_title">Présentation</h2>
           <p className="about_main_text">
             Bonjour, je m'appelle Fabien Costa, développeur front-end spécialisé
@@ -25,30 +22,29 @@ const About = () => {
             approfondir mes compétences en React.
           </p>
 
-          {/* Affiche le contenu additionnel si isExpanded est true */}
-          {isExpanded && (
-            <>
-              <p className="about_main_text">
-                Bien que je me concentre sur le front-end, j'ai aussi des bases
-                en Node.js, ce qui me donne une vision globale du développement
-                web. En plus de mes compétences techniques, je suis autonome,
-                ouvert d'esprit avec une bonne capacité à résoudre des
-                problèmes.
-              </p>
-              <p className="about_main_text">
-                Je suis adaptable, ce qui me permet de m'ajuster rapidement aux
-                nouvelles technologies et environnements de travail. J'apprécie
-                aussi le travail en équipe pour atteindre des objectifs communs.
-              </p>
-              <p className="about_main_text">
-                L'objectif de cette présentation est de montrer que, malgré mon
-                statut de jeune développeur, je suis déjà autonome pour occuper
-                un poste de front-end. Je recherche mon premier emploi, mais je
-                suis aussi ouvert à un stage pour prouver que je peux être un
-                atout pour votre équipe.
-              </p>
-            </>
-          )}
+          {/* Section additionnelle avec un smooth collapse */}
+          <div
+            className={`about_main_collapse ${isExpanded ? "expanded" : ""}`}
+          >
+            <p className="about_main_text">
+              Bien que je me concentre sur le front-end, j'ai aussi des bases en
+              Node.js, ce qui me donne une vision globale du développement web.
+              En plus de mes compétences techniques, je suis autonome, ouvert
+              d'esprit avec une bonne capacité à résoudre des problèmes.
+            </p>
+            <p className="about_main_text">
+              Je suis adaptable, ce qui me permet de m'ajuster rapidement aux
+              nouvelles technologies et environnements de travail. J'apprécie
+              aussi le travail en équipe pour atteindre des objectifs communs.
+            </p>
+            <p className="about_main_text">
+              L'objectif de cette présentation est de montrer que, malgré mon
+              statut de jeune développeur, je suis déjà autonome pour occuper un
+              poste de front-end. Je recherche mon premier emploi, mais je suis
+              aussi ouvert à un stage pour prouver que je peux être un atout
+              pour votre équipe.
+            </p>
+          </div>
 
           {/* Bouton pour afficher ou réduire le contenu additionnel */}
           <button className="about_main_button" onClick={handleExpand}>
@@ -56,7 +52,6 @@ const About = () => {
           </button>
         </div>
 
-        {/* Section des formations */}
         <div className="about_main_description">
           <h3 className="about_main_title">Formations</h3>
           <p className="about_main_text">
